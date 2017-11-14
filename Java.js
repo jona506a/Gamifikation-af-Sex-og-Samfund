4a851d4dcbfef95f03df80c01b22542a8abe1af1
Frederikerichsen <fred356e@stud.kea.dk>
2017-11-14 13:00:20 +0100

    Game - Animation click - Added

--- Java.js
+++ Java.js
@@ -1,29 +1,107 @@
 $(window).on("load", StartStory);
 var niveau1_klik = 0;
 var er_niveau1_faerdig = false;
+var er_den_passende;
+var the_score = 0;
+var theTimer;
+var svar_tid = 10000;
 
 function StartStory() {
-    console.log("Historien er startet");
+    //  console.log("Historien er startet");
     $("#mrpickle_sprite").addClass("mrpickle_sprite");
-    niveau1();
-
+    startSpm();
 }
 
+function startSpm() {
 
-function niveau1() {
-    niveau1_klik++;
-    console.log("Niveau 1 - start");
-    if (niveau1_klik == 4) {
-        er_niveau1_faerdig = true;
+    console.log("START SPØrgsmål the_score", the_score);
+    knapperOff();
+    clearTimeout(theTimer);
+    if (niveau1_klik == 9) {
+        console.log("Spillet er Slut!");
 
-        niveau2();
+    } else {
+        niveau1();
+        theTimer = setTimeout(tidenErGaaet, svar_tid);
+
     }
 
-    $("#knap_del").on("click", randomValg1);
-    $("#knap_del_ikke").on("click", randomValg1);
-    // setTimeout(randomValg1, 3000);
+
+
 }
 
+function tidenErGaaet() {
+    console.log("tiden er gået");
+
+
+
+    startSpm();
+
+
+
+
+
+}
+
+function niveau1() {
+
+    niveau1_klik++;
+    console.log("niveau1_klik", niveau1_klik);
+    if (niveau1_klik == 4) {
+        // timertiden skal sættes ned
+        svar_tid = 5000;
+
+    } else if (niveau1_klik == 7) {
+        svar_tid = 1000;
+
+    }
+    console.log("svar_tid", svar_tid);
+
+    randomValg1();
+    $("#knap_del").on("click", del_klik);
+    $("#knap_del_ikke").on("click", del_ikke_klik);
+
+
+
+
+}
+
+function del_klik() {
+
+    // $("#passende_sprite").addClass("out_left");
+
+    if (er_den_passende == true) {
+
+        the_score++;
+
+    } else {
+
+    }
+    styr_mr_pickle();
+    startSpm();
+}
+
+function del_ikke_klik() {
+
+    // $("#upassende_sprite").addClass("out_right");
+
+    if (er_den_passende == true) {
+
+
+
+    } else {
+        the_score++;
+    }
+    styr_mr_pickle();
+    startSpm();
+}
+
+function styr_mr_pickle() {
+
+    //Logik til mr Pickles spritesheet
+    //    if else if etc.
+
+}
 
 function randomValg1() {
     console.log("random valg");
@@ -33,8 +111,10 @@ function randomValg1() {
     // var random = 1;
     if (random > 0.5) {
         upassende();
+        er_den_passende = false;
     } else {
         passende();
+        er_den_passende = true;
     }
 }
 /* -----------PASSENDE BILLEDER----------- */
@@ -48,7 +128,8 @@ function upassende() {
     console.log("mitRandomTalX", mitRandomTalX);
     console.log("mitRandomTalY", mitRandomTalY);
     $("#upassende_sprite").css("background-position", (-mitRandomTalX * 100) + "% " + (-mitRandomTalY * 100) + "%");
-    niveau1_restart();
+
+    //  niveau1_restart();
 }
 /* -----------UPASSENDE BILLEDER----------- */
 
@@ -63,20 +144,28 @@ function passende() {
     console.log("mitRandomTalY", mitRandomTalY);
     $("#passende_sprite").css("background-position", (-mitRandomTalX * 100) + "% " + (-mitRandomTalY * 100) + "%");
 
-    niveau1_restart();
+    //  niveau1_restart();
 }
 
-function niveau1_restart() {
-    console.log("neveau1_restart")
-    $("#knap_del").off("click", randomValg1);
-    $("#knap_del_ikke").off("click", randomValg1);
-    setTimeout(niveau1, 2000);
-    //niveau1();
+//function niveau1_restart() {
+//    console.log("neveau1_restart")
+//    $("#knap_del").off("click", randomValg1);
+//    $("#knap_del_ikke").off("click", randomValg1);
+//    setTimeout(niveau1, 2000);
+//    //niveau1();
+//}
+
+function knapperOff() {
+    $("#knap_del").off("click");
+    $("#knap_del_ikke").off("click");
+
 }
 
 //---- NIVEAU2 START -----
+/*
 function niveau2() {
 
     console.log("Niveau 2 - start");
 
 }
+*/
